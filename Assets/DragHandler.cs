11d8342f7cdbb2e53457a7854235151bd2926bb2
mwbullen <mwbullen@gmail.com>
2017-07-11 14:43:14 -0400

    start mouse drag and drop

---
+++ Assets/DragHandler.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+	Vector3 startPosition;	
+	#region IBeginDragHandler implementation
+
+	public void OnBeginDrag (PointerEventData eventData)
+	{
+		startPosition = transform.position;
+	}
+
+	#endregion
+
+	#region IDragHandler implementation
+
+	public void OnDrag (PointerEventData eventData)
+	{
+		transform.position = Input.mousePosition;
+	}
+
+	#endregion
+
+	#region IEndDragHandler implementation
+
+	public void OnEndDrag (PointerEventData eventData)
+	{
+		transform.position = startPosition;
+	}
+
+	#endregion
+
+
+
+}
